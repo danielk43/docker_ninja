3435b0ffbd257d8fdadb9905383d8123aab9db94
danielk43 <11856117+danielk43@users.noreply.github.com>
2024-10-31 04:18:00 -0400

    Add 9th gen to vendor boot builds

--- build_android.sh
+++ build_android.sh
@@ -625,11 +625,11 @@ do
     fi
 
     # Build
-    if grep -q "${device}" <<< "husky shiba akita cheetah panther lynx tangorpro felix raven oriole bluejay"
+    if grep -q "${device}" <<< "comet komodo caimen tokay husky shiba akita cheetah panther lynx tangorpro felix raven oriole bluejay"
     then
       m vendorbootimage
     fi
-    if grep -q "${device}" <<< "husky shiba akita cheetah panther lynx tangorpro felix"
+    if grep -q "${device}" <<< "comet komodo caimen tokay husky shiba akita cheetah panther lynx tangorpro felix"
     then
       m vendorkernelbootimage
     fi
